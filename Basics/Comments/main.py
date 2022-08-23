b03ca73d890a787c43fcd8fc3c7d11d470524f58
parrarodrigueze <esteban.parrarodriguez@belmont.edu>
2022-08-23 13:50:44 -0500

    Updated dependencies

--- Basics/Comments/main.py
+++ Basics/Comments/main.py
@@ -8,7 +8,8 @@ print("# this is not a comment")
 a = 10  # This is an inline comment, code before the comment will run
 # print(a) . No code on this line will run
 
-# print(a) # If you remove th first #, you will uncomment the code. This text will remain commented
+# print(a) # If you remove the first #, you will uncomment the code. This text will remain commented
+
 
 # The following line should be commented:
 # print("This line should not be printed!")
